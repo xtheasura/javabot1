56c40fef1201fb4fe320234a16945056ebce8883
theblacklinen <luckychauhan653@gmail.com>
2023-04-02 12:29:01 +0530

    First commit

---
+++ src/main/java/com/example/bot.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public  class bot extends TelegramLongPollingBot{
+
+    
+
+    @Override
+    public void onUpdateReceived(Update update) {
+       if(update.hasMessage()){
+        if(update.getMessage().getText().equals("/start")){
+            String text = "Hello" + update.getMessage().getFrom().getFirstName();
+            SendMessage message = new SendMessage(update.getMessage().getChatId().toString(),text);
+             try {
+                execute(message);
+                
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+
+            }
+        }
+       }
+    
+
+    @Override
+    public String getBotUsername() {
+        return"javapractice1bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return"6198452172:AAGY_ul6jOQpwFt73LQb8fvOkrD46adKeNM";
+    }
+}
+
+
+
+    
+
